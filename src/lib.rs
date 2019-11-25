cb64e291cbfd0e091b2313487266224c2f195873
Josh Mcguigan <joshmcg88@gmail.com>
2019-11-25 04:03:07 -0800

    wip impl read for tee

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
 use std::{io, io::{Read, Write}};
 
-struct Tee<R, W> {
+pub struct Tee<R, W> {
     reader: R,
     writer: W,
+    buf: [u8; 8192],
+    len: usize,
 }
 
 impl<R, W> Tee<R, W> {
-    fn new(reader: R, writer: W) -> Self {
-        Self { reader, writer }
+    pub fn new(reader: R, writer: W) -> Self {
+        Self { reader, writer, buf: [0; 8192], len: 0 }
     }
 
-    fn into_inner(self) -> (R, W) {
-        (self.reader, self.writer)
+    #[cfg(test)]
+    fn get_writer_ref(&self) -> &W {
+        &self.writer
     }
 }
 
@@ -20,8 +23,24 @@ impl<R, W> Read for Tee<R, W>
           W: Write
 {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let len = self.reader.read(buf)?;
-        self.writer.write(&buf[0..len]);
+        let total_len = self.reader.read(&mut self.buf)?;
+        let newline_index = self.buf[0..total_len].iter().position(|b| *b == '\n' as u8);
+        let len = if let Some(newline_index) = newline_index {
+            newline_index + 1
+        } else {
+            total_len
+        };
+        self.writer.write(&self.buf[0..len])?;
+        &mut buf[0..len].copy_from_slice(&self.buf[0..len]);
+
+        if len < total_len {
+            // This means we didn't write out all of the bytes we got in. This is done
+            // to allow the reader a chance to process each line before we print any bytes
+            // on the next line, which gives the reader a chance to write their own bytes to
+            // standard out, without interleaving.
+
+            // copy total_len - len elements to start of self.buf, then set self.len
+        }
 
         Ok(len)
     }
@@ -64,12 +83,12 @@ mod tests {
     }
 
     #[test]
-    fn single_line() {
+    fn single_read() {
         let std_in = vec![
             String::from("testing"),
         ];
-        let mut mock_std_in = MockStdIn::new(std_in);
-        let mut mock_std_out = vec![];
+        let mock_std_in = MockStdIn::new(std_in);
+        let mock_std_out = vec![];
 
         let mut tee = Tee::new(mock_std_in, mock_std_out);
 
@@ -77,7 +96,50 @@ mod tests {
         assert_eq!(7, tee.read(&mut buf).unwrap());
         assert_eq!(b"testing", &buf[0..7]);
 
-        let (_, mock_std_out) = tee.into_inner();
+        let mock_std_out = tee.get_writer_ref();
         assert_eq!(b"testing", &mock_std_out[0..7]);
+    }
+
+    #[test]
+    fn single_read_ends_in_newline() {
+        let std_in = vec![
+            String::from("testing\n"),
+        ];
+    }
+
+    // TODO handle windows newlines
+
+    #[test]
+    fn single_read_with_newline() {
+        let std_in = vec![
+            String::from("testing\nis great"),
+        ];
+        let mock_std_in = MockStdIn::new(std_in);
+        let mock_std_out = vec![];
+
+        let mut tee = Tee::new(mock_std_in, mock_std_out);
+
+        let mut buf = [0; 100];
+        assert_eq!(8, tee.read(&mut buf).unwrap());
+        assert_eq!(b"testing\n", &buf[0..8]);
+
+        let mock_std_out = tee.get_writer_ref();
+        assert_eq!(b"testing\n", &mock_std_out[0..8]);
+
+        assert_eq!(8, tee.read(&mut buf).unwrap());
+        assert_eq!(b"is great", &buf[0..8]);
+
+        let mock_std_out = tee.get_writer_ref();
+        assert_eq!(b"is great", &mock_std_out[0..8]);
+    }
+
+    #[test]
+    fn multiline() {
+        let std_in = vec![
+            String::from("testing.."),
+            String::from("."),
+            String::from(".\n."),
+            String::from("is fun"),
+        ];
     }
 }
